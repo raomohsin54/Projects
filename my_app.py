b0e192d0c516b34cdc5b77afbe42fd7ad36fb631
Mohsin Mukhtiar <rao.mohsin.54@gmail.com>
2023-04-14 12:03:01 +0800

    Test

--- my_app.py
+++ my_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 def app():
     st.set_page_config(page_title="Pairplot using Streamlit", page_icon=":bar_chart:")
@@ -8,9 +9,16 @@ def app():
     # Load dataset
     df = sns.load_dataset("penguins")
 
-    # Set style and plot
-    sns.set_style("ticks")
-    graph = sns.pairplot(df, hue="species")
+    # Show a progress bar while the visualization is loading
+    with st.spinner("Loading Visualization..."):
+        # Set style and plot
+        sns.set_style("ticks")
+        graph = sns.pairplot(df, hue="species")
 
     # Show the plot using Streamlit
     st.pyplot(graph.fig)
+
+if __name__ == '__main__':
+    app()
+
+
