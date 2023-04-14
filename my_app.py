aa405160ef5370f443ed9dc4622f2323784ed98d
Mohsin Mukhtiar <rao.mohsin.54@gmail.com>
2023-04-14 12:39:14 +0800

    Test

--- my_app.py
+++ my_app.py
@@ -1,34 +1,33 @@
 import streamlit as st
-import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Set up the game
-def setup():
-    st.write("# Guess the number!")
-    st.write("I'm thinking of a number between 1 and 100.")
-    st.write("Can you guess what it is?")
-    return random.randint(1, 100)
+# Generate random data
+np.random.seed(42)
+data = pd.DataFrame({
+    'x': np.random.normal(0, 1, 100),
+    'y': np.random.normal(0, 1, 100)
+})
 
-# Main game loop
-def game():
-    number = setup()
-    guess = None
-    num_guesses = 0
+# Visualize data using a scatter plot
+fig, ax = plt.subplots()
+sns.scatterplot(x='x', y='y', data=data, ax=ax)
+ax.set_title('Scatter plot of x vs. y')
+st.pyplot(fig)
 
-    while guess != number:
-        guess = st.number_input("Enter your guess:", min_value=1, max_value=100)
-        num_guesses += 1
+# Visualize data using a histogram
+fig, ax = plt.subplots()
+sns.histplot(data['y'], bins=10, ax=ax)
+ax.set_title('Histogram of y')
+st.pyplot(fig)
 
-        if guess < number:
-            st.write("Too low, try again!")
-        elif guess > number:
-            st.write("Too high, try again!")
-        else:
-            st.write(f"Congratulations, you guessed it in {num_guesses} tries!")
-
-# Start the game
-game()
-
-
+# Visualize data using a box plot
+fig, ax = plt.subplots()
+sns.boxplot(y='y', data=data, ax=ax)
+ax.set_title('Box plot of y')
+st.pyplot(fig)
 
 if __name__ == '__main__':
     app()
